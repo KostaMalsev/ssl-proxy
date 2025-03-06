14c097c380371df4bba07b1a553ca72cda5f4bf3
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2025-03-06 18:16:44 +0200

    Update clients.js

--- api/clients.js
+++ api/clients.js
@@ -1,1 +1,11 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("http://18.207.232.153:3003/clients")
+    const data = await response.text()
+    res.status(response.status).send(data)
+  } catch (error) {
+    res.status(500).json({ error: "Failed to proxy request" })
+  }
+}
+
 
